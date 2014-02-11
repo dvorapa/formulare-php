712b6b86da10e8e95eec0dd71224d6215406d4cc
Pavel Dvořák <dvorapa@seznam.cz>
2014-02-11 06:29:45 +0100

    Šablona a výsledek první soubor (jen rozvržení)

---
+++ Nova/Nova.php
@@ -0,0 +1,20 @@
+<?php session_start(); ?>
+<!Doctype Html>
+<Html Lang="Cs">
+<Head>
+<Meta Charset="Utf-8">
+<Meta Name="Application-name" Content="Přihláška na VŠ">
+<Meta Name="Author" Content="Pavel Dvořák, 8.O">
+<Meta Name="Description" Content="Online přihláška ke studiu na vysoké škole vytvořená ke zjednodušení vyplňování, tisku a následném zasílání">
+<Meta Name="Keywords" Content="přihláška,VŠ,formuláře,PHP,vysoké školy">
+<Meta Name="Robots" Content="Follow,index">
+<Link Type="Image/x-icon" Rel="Shortcut icon" Href="/favicon.ico">
+<Style Type="Text/css">
+
+</Style>
+<Title>Přihláška</Title>
+</Head>
+<Body>
+
+</Body>
+</Html>
---
+++ Nova/Vysledek.php
@@ -0,0 +1,46 @@
+<?php session_start(); ?>
+<!Doctype Html>
+<Html Lang="Cs">
+<Head>
+<Meta Charset="Utf-8">
+<Meta Name="Application-name" Content="Přihláška na VŠ">
+<Meta Name="Author" Content="Pavel Dvořák, 8.O">
+<Meta Name="Description" Content="Online přihláška ke studiu na vysoké škole vytvořená ke zjednodušení vyplňování, tisku a následném zasílání">
+<Meta Name="Keywords" Content="přihláška,VŠ,formuláře,PHP,vysoké školy">
+<Meta Name="Robots" Content="Follow,index">
+<Link Type="Image/x-icon" Rel="Shortcut icon" Href="/favicon.ico">
+<Link Type="Text/css" Rel="Stylesheet" Href="/Aplikace/mfglabs_iconset.css">
+<Link Type="Text/css" Rel="Stylesheet" Href="http://fonts.googleapis.com/css?family=Ubuntu|Open+Sans&subset=latin,latin-ext">
+<Link Type="Text/css" Rel="Stylesheet" Href="/Styly.css">
+<Style Type="Text/css">
+@media{
+Body{Background-color:#FFBF00;Color:#242424}
+A{Color:#737373}
+Form{Background-color:white}
+}
+</Style>
+<!--[if lt IE 9]>
+<Script Type="Application/javascript" Src="Aplikace/html5shiv.js"></Script>
+<![endif]-->
+<Title>Přihláška</Title>
+</Head>
+<Body>
+<Nav>
+<H1></H1>
+<A Href="#" Title=""><I Class="icon-"></I></A>
+</Nav>
+
+
+
+<Form Action="Zpracovani.php" Method="Post">
+
+</Form>
+
+
+
+<Nav>
+<H1></H1>
+<A Href="" Title=""><I Class="icon-"></I></A>
+</Nav>
+</Body>
+</Html>
