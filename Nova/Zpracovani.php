fe06c5f10567e12a4e889776e2b17c2a3245dee1
Pavel Dvořák <dvorapa@seznam.cz>
2014-02-14 23:42:55 +0100

    4. oprava SQL

--- Nova/Zpracovani.php
+++ Nova/Zpracovani.php
@@ -63,6 +63,7 @@ $_SESSION[$Promenna]=$_POST[$Promenna];
 /*     /                         */
 foreach($_POST as $Kolekce){
 if((is_array($Kolekce))&&(array_key_exists("Kam",$Kolekce))){
+$_SESSION["Pole"][]=session_id();
 
 /* ___                                 */
 /*  _/ _/ _ . _   _| _    _ _  _ | . _ */
@@ -81,9 +82,9 @@ setcookie($Promenna,$_SESSION[$Promenna],$_SESSION["d"]);
 /*       |                                     */
 if(array_key_exists("Databaze",$Kolekce)){
 $Databaze=mysqli_connect("localhost","dvorapa","cepetauhacac","databazeprihlasek");
-mysqli_query($Databaze,"insert into Prihlasky (PHPSESSID) values ('".session_id()."')");
 foreach($_SESSION["Pole"] as $Promenna){
 if(!empty($_POST[$Promenna])){
+$_SESSION[$Promenna]=mysqli_real_escape_string($Databaze,$_SESSION[$Promenna]);
 mysqli_query($Databaze,"insert into Prihlasky (".$Promenna.") values (".$_SESSION[$Promenna].")");
 }}
 mysqli_close($Databaze);
