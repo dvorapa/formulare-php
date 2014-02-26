97687e8e324fe4112c9cf134501514d6b648cf47
Pavel Dvořák <dvorapa@seznam.cz>
2014-02-26 18:58:38 +0100

    ještě předloha (aspoň v základu)

---
+++ nova/predloha.php
@@ -0,0 +1,51 @@
+<?php session_start(); ?>
+<!Doctype Html>
+<Html Lang="Cs">
+<Head>
+<Meta Charset="Utf-8">
+<Meta Name="Application-name" Content="Přihláška na VŠ">
+<Meta Name="Author" Content="Pavel Dvořák, 8.O">
+<Meta Name="Description" Content="Online přihláška ke studiu na vysoké škole vytvořená ke zjednodušení vyplňování, tisku a následném zasílání">
+<Meta Name="Keywords" Content="přihláška,VŠ,formuláře,PHP,vysoké školy">
+<Meta Name="Robots" Content="Follow,index">
+<Meta Name="Viewport" Content="width=device-width">
+<Link Type="Image/x-icon" Rel="Shortcut icon" Href="/favicon.ico">
+<Style Type="Text/css">
+Body{Margin:0;Height:1188mm;Width:210mm}
+Div{Height:297mm;Width:210mm}
+Table{Background-color:#FDB850;Margin:9.5mm 13.75mm;Padding-top:5mm;Padding-left:1.5mm;Padding-bottom:6mm;Padding-right:1.5mm;Height:278mm;Width:182.5mm}
+</Style>
+<Title>Přihláška</Title>
+</Head>
+<Body>
+<Div>
+<Table>
+
+</Table>
+</Div>
+
+
+
+<Div>
+<Table>
+
+</Table>
+</Div>
+
+
+
+<Div>
+<Table>
+
+</Table>
+</Div>
+
+
+
+<Div>
+<Table>
+
+</Table>
+</Div>
+</Body>
+</Html>
