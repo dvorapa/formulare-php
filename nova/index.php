5fbd6cdffcaa73646f4a5dfc7bc3801ccfb19537
Pavel Dvořák <dvorapa@seznam.cz>
2014-03-05 17:59:21 +0100

    11. návrh přesměrování

--- nova/index.php
+++ nova/index.php
@@ -57,9 +57,9 @@ $_SESSION["k"]=0;
 <Body>
 <Nav>
 <H1></H1>
-<Button Form="Formular" Name="Prejit[Kam]" Type="Submit" Value="./#<?php echo $_SESSION["PoleStran"][$_SESSION["k"]--][1]; ?>">Zpět</Button>
+<Button Form="Formular" Name="Prejit[Kam]" Type="Submit" Value="./#<?php $_SESSION["k"]--;echo $_SESSION["PoleStran"][$_SESSION["k"]][1]; ?>">Zpět</Button>
 <Br>
-<Button Form="Formular" Name="Prejit[Kam]" Type="Submit" Value="./#<?php echo $_SESSION["PoleStran"][$_SESSION["k"]++][1]; ?>">Dále</Button>
+<Button Form="Formular" Name="Prejit[Kam]" Type="Submit" Value="./#<?php $_SESSION["k"]++;echo $_SESSION["PoleStran"][$_SESSION["k"]][1]; ?>">Dále</Button>
 <Br>
 <Input Form="Formular" Name="Ulozit[Cookie]" Type="Hidden" Value="1">
 <Input Form="Formular" Name="Ulozit[Databaze]" Type="Hidden" Value="true">
