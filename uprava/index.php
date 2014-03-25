6bbb70a36b193ba57d629f0bf8dac9036e485b17
Pavel Dvořák <dvorapa@seznam.cz>
2014-03-25 02:15:54 +0100

    Vzhled + úprava PHP
    Funkčnost = 0 : oprava příště (snad)

--- uprava/index.php
+++ uprava/index.php
@@ -1,4 +1,7 @@
-<?php session_start(); ?>
+<?php
+$_SESSION["SesID"]=session_id()
+session_start();
+?>
 <!Doctype Html>
 <Html Lang="Cs">
 <Head>
@@ -13,16 +16,17 @@
 <Link Rel="Stylesheet" Href="/aplikace/mfglabs_iconset.css">
 <Link Type="Text/css" Rel="Stylesheet" Href="http://fonts.googleapis.com/css?family=Ubuntu|Open+Sans&subset=latin,latin-ext">
 <Style Type="Text/css">
-Body{Background-color:#FFBF00;Color:#242424;Font-family:Ubuntu,"Open Sans",sans-serif;Margin:0 auto;Position:absolute;Left:10%;Right:10%}
-H1{Margin:0.5em 0}
+Body{Background-color:#FFBF00;Color:#242424;Font-family:Ubuntu,"Open Sans",sans-serif;Margin:0}
 A{Color:#242424;Text-decoration:none}
-A:hover,A:active{Text-shadow:0 0 0.5em}
-A.Tlacitko{Background-color:#242424;Border-radius:0.25em;Color:#FFBF00;Display:inline-block;Margin:1em 0;Margin-right:1em;Padding:0.5em 2em;Text-decoration:none;Text-transform:uppercase}
-A.Tlacitko:hover,A.Tlacitko:active{Box-shadow:0 0 0.5em #242424}
+Button{Background-color:#242424;Border:0;Border-radius:0.25em;Color:#FFBF00;Cursor:pointer;Font-size:1em;Margin:1em 0;Padding:0.5em 2em;Text-transform:uppercase}
+A:hover,A:active,Button:hover,Button:active{Text-shadow:0 0 0.5em}
+Button:hover,Button:active{Box-shadow:0 0 0.5em #242424}
+Form{Margin:auto;Position:absolute;Left:10%;Right:10%}
+Header H1{Margin:0.5em 0}
+Input{Border:1px solid silver;Border-radius:0.25em;Box-shadow:inset 0 1px 1px rgba(0,0,0,0.1);Padding:0.5em;Width:18em}
 
 @media (orientation:landscape){
-Body{Left:18%;Right:18%}
-P,A{Font-size:1.2em}
+Form{Left:18%;Right:18%}
 Footer{Margin:auto;Position:absolute;Z-index:-1;Left:0;Bottom:0;Right:0}
 }
 </Style>
@@ -36,9 +40,9 @@ Footer{Margin:auto;Position:absolute;Z-index:-1;Left:0;Bottom:0;Right:0}
 <H1><I Class="icon-magnifying icon3x"></I><Br>Upravit</H1>
 Vložte prosím svůj kód:
 <Br>
-<Input Name="" Type="Text" Value="<?php echo session_id(); ?>">
+<Input Name="" Type="Text" Value="<?php echo !empty($_SESSION["SesID"])?$_SESSION["SesID"]:""; ?>">
 <Input Name="" Type="Text" Value="<?php echo !empty($_SESSION["RCislo"])?$_SESSION["RCislo"]:""; ?>">
-<Button Name="Prejit[Kam]" Type="Submit" Value="nova/<?php echo $_SESSION["c"]; ?>">Načíst</Button>
+<Button Name="" Type="Submit" Value="nova/?">Načíst</Button>
 </Header>
 </Form>
 
