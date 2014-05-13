87cc12ef273aa3b2cd9d78813fbbf044bda2fa5b
Pavel Dvořák <dvorapa@seznam.cz>
2014-05-13 22:01:12 +0200

    Oprava u jedné proměnné, budou třeba další?

--- aplikace/htmltodocx/htmltodocx.php
+++ aplikace/htmltodocx/htmltodocx.php
@@ -14,6 +14,9 @@ class HtmlToDocx{
 
 public $base_path;
 public $base_root;
+public $html;
+public $html_array;
+public $PHPWord;
 
 /// __construct
 public function __contruct(){
@@ -43,49 +46,51 @@ $this->process();
 
 protected function process(){
 // Create a new PHPWord Object
-$PHPWord = new PHPWord();
+$PHPWord=new PHPWord();
 
 // Every element you want to append to the word document is placed in a section. So you need a section:
-$section = $PHPWord->createSection();
+$section=$PHPWord->createSection();
 
-$this->base_path = $base_path;
-$this->base_root = $base_root;
+$this->base_path=$base_path;
+$this->base_root=$base_root;
 
 // Provide some initial settings:
-$initial_state = array(
+$initial_state=array(
 // Required parameters:
-'phpword_object' => &$PHPWord, // Must be passed by reference.
-'base_root' => $base_root, // Required for link elements - change it to your domain.
-'base_path' => $base_path, // Path from base_root to whatever url your links are relative to.
+'phpword_object'=>&$PHPWord, // Must be passed by reference.
+'base_root'=>$base_root, // Required for link elements - change it to your domain.
+'base_path'=>$base_path, // Path from base_root to whatever url your links are relative to.
 
 // Optional parameters - showing the defaults if you don't set anything:
-'current_style' => array('size' => '11'), // The PHPWord style on the top element - may be inherited by descendent elements.
-'parents' => array(0 => 'body'), // Our parent is body.
-'list_depth' => 0, // This is the current depth of any current list.
-'context' => 'section', // Possible values - section, footer or header.
-'pseudo_list' => TRUE, // NOTE: Word lists not yet supported (TRUE is the only option at present).
-'pseudo_list_indicator_font_name' => 'Wingdings', // Bullet indicator font.
-'pseudo_list_indicator_font_size' => '7', // Bullet indicator size.
-'pseudo_list_indicator_character' => 'l ', // Gives a circle bullet point with wingdings.
-'table_allowed' => TRUE, // Note, if you are adding this html into a PHPWord table you should set this to FALSE: tables cannot be nested in PHPWord.
-'treat_div_as_paragraph' => TRUE, // If set to TRUE, each new div will trigger a new line in the Word document.
+'current_style'=>array('size'=>'11'), // The PHPWord style on the top element - may be inherited by descendent elements.
+'parents'=>array(0=>'body'), // Our parent is body.
+'list_depth'=>0, // This is the current depth of any current list.
+'context'=>'section', // Possible values - section, footer or header.
+'pseudo_list'=>TRUE, // NOTE: Word lists not yet supported (TRUE is the only option at present).
+'pseudo_list_indicator_font_name'=>'Wingdings', // Bullet indicator font.
+'pseudo_list_indicator_font_size'=>'7', // Bullet indicator size.
+'pseudo_list_indicator_character'=>'l ', // Gives a circle bullet point with wingdings.
+'table_allowed'=>TRUE, // Note, if you are adding this html into a PHPWord table you should set this to FALSE: tables cannot be nested in PHPWord.
+'treat_div_as_paragraph'=>TRUE, // If set to TRUE, each new div will trigger a new line in the Word document.
 
 // Optional - no default:
-'style_sheet' => htmltodocx_styles_example(), // This is an array (the "style sheet") - returned by htmltodocx_styles_example() here (in styles.inc) - see this function for an example of how to construct this array.
+'style_sheet'=>htmltodocx_styles_example() // This is an array (the "style sheet") - returned by htmltodocx_styles_example() here (in styles.inc) - see this function for an example of how to construct this array.
 );
 
 // Create a DOM object
-$html = new simple_html_dom();
+$html=new simple_html_dom();
 }
 
 /// input_string
 public function input_string(&$input_string){
+$html=$this->html;
+
 // Load HTML from a string
 load_string:{
 $html->load($input_string);
 
 // Create the dom array of elements which we are going to work on:
-$html_array = $html->find('body');
+$html_array=$html->find('body');
 
 if(empty($html_array)){
 $input_string="<body>".$input_string."</body>";
@@ -94,12 +99,14 @@ goto load_string;
 
 /// input_file
 public function input_file($input_file){
+$html=$this->html;
+
 // Load HTML from a URL or a HTML file
 $html->load_file($input_file);
 
 // Create the dom array of elements which we are going to work on:
 load_file:{
-$html_array = $html->find('body');
+$html_array=$html->find('body');
 
 if(empty($html_array)){
 $html->outertext="<body>".$html->outertext."</body>";
@@ -108,14 +115,16 @@ goto load_file;
 
 /// output_file
 public function output_file(array $output_file){
-$base_output_file=array(tempnam(sys_get_temp_dir(), 'htd'));
+$html=$this->html;
+
+$base_output_file=array(tempnam(sys_get_temp_dir(),'htd'));
 array_merge($base_output_file,$output_file);
 
 // Convert the HTML and put it into the PHPWord object
-htmltodocx_insert_html($section, $html_array[0]->nodes, $initial_state);
+htmltodocx_insert_html($section,$html_array[0]->nodes,$initial_state);
 
 // At least write the document to webspace:
-$objWriter = PHPWord_IOFactory::createWriter($PHPWord, 'Word2007');
+$objWriter=PHPWord_IOFactory::createWriter($PHPWord,'Word2007');
 $objWriter->save($output_file);
 }
 
@@ -129,14 +138,14 @@ header('Content-Transfer-Encoding: binary');
 header('Expires: 0');
 header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 header('Pragma: public');
-header('Content-Length: ' . filesize($output_file));
+header('Content-Length: '.filesize($output_file));
 }
 
 /// close
 public function close(){
 // Clear the HTML dom object:
-$html->clear();
-unset($html);
+$this->html->clear();
+unset($this->html);
 $this->base_path=null;
 unset($this->base_path);
 $this->base_root=null;
